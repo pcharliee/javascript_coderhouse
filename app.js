506a446e25da54beccfbc76c6197cb2d8aa0f40e
Carlos Delgado <carlosm.delgador@gmail.com>
2021-06-16 10:22:12 -0300

    Desafio 2 - algoritmo con condicionales

--- app.js
+++ app.js
@@ -1,31 +1,35 @@
-// alert('Bienvenido a la primera prueba. A continuación, te voy a pedir una serie de datos')
-// let nombre = prompt('Ingresa tu nombre');
-// let edad = parseInt(prompt('Cuantos años tienes?'));
-// let destino = prompt('Destino de vacaciones favorito?')
-
-// let acertijo = confirm('Pesa más un kilo de acero que un kilo de algodón?')
-
-// alert(`
-//   ${nombre}, el acertijo que respondiste esta pensado para niños... 
-//   Según me cuentas, tu tienes ${edad} años. Abre la consola para saber tu resultado!
-// `) 
-
-// acertijo
-// ? console.log(`${nombre}, tu resultado fue ${acertijo}! Te has ganado un pasaje a ${destino}. FELICIDADES!`) 
-// : console.log(`${nombre}, tu resultado fue ${acertijo}! Refresca la página para intentar de nuevo`)
-
 ///////////////////
 ///CONDICIONALES///
 ///////////////////
 
-let numero1 = parseInt(prompt('Ingresa un numero'))
-let numero2 = parseInt(prompt('Ingresa otro numero'))
-let calculo = numero1 * numero2
+let error, coterraneo;
 
-if(calculo > 100) {
-  console.log(`mayor a 100. tu calculo fue ${calculo}`)
-} else {
-  console.log(`menor a 100. tu calculo fue ${calculo}`)
+let nombre = prompt('Hola! Como te llamas?').toUpperCase()
+alert(`Mucho gusto ${nombre}. Perdon que grite, es que me alegra que estes aca!`)
+let edad = parseInt(prompt('Cuantos años tienes?'))
+let destino = prompt('De donde eres?').toLowerCase()
+let sugerencia;
+
+if (destino == "venezuela") { 
+  alert(`Que interesante, yo tambien soy de ${destino}`) 
+  coterraneo = false;
+} else { 
+  alert(`Que interesante, nunca fui a ${destino}`) 
+  coterraneo = true
 }
 
-document.getElementById("resultado").innerHTML = calculo
+if (edad < 18) {
+  sugerencia = `Aprovecha que aun tienes ${edad} años, mira que despues tienes que trabajar :P`
+} else if (edad >= 18) {
+  sugerencia = `A seguir dandole a la vida, lo que te propongas lo lograrás :)`
+}
+
+alert(`Ya me canse de aparecer y desaparecer a modo de 'alert', lo próximo lo voy a escribir en el HTML :)`);
+
+let resultado = `
+  Muchas gracias por pasar ${nombre.toLowerCase()}. Espero algún día conocer ${destino}.
+  En vista de que tienes ${edad} años, te dejo un consejo: ${sugerencia}.
+  Nos vemos pronto!
+`;
+
+document.getElementById("resultado").innerHTML = resultado
