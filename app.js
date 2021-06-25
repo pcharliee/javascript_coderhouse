e67e202dc58c65c99602c6b7f5ce28a2ff74179a
Carlos Delgado <carlosm.delgador@gmail.com>
2021-06-25 05:10:14 -0300

    progreso desafio funciones

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
 ///////////////////
-//////BUCLES///////
+/////FUNCIONES/////
 ///////////////////
 
-let input = prompt("Asi que quieres ser un Mortífago... ¿Cómo te llamas?").toLowerCase();
-let primeraLetra = input.substr(0, 1).toUpperCase();
-let nombre = `${input.substr(0, 1).toUpperCase()}${input.substr(1)}`
+// let input = prompt("Asi que quieres ser un Mortífago... ¿Cómo te llamas?").toLowerCase();
+// let primeraLetra = input.substr(0, 1).toUpperCase();
+// let nombre = `${input.substr(0, 1).toUpperCase()}${input.substr(1)}`
 
-for(let i = 3; i > 0; i--) {
-  alert(`Preparate para responder nuestra única pregunta en ... ${i}`)
-};
+// let nombre = prompt('')
+let comprar = document.getElementById('comprar').value
+console.log(comprar)
 
-let darkLord;
-let count = 0; 
-let resultado;
-let intentos = `${nombre}, tus intentos fueron:`;
+let boton = document.getElementById('comprar')
 
+boton.innerHTML = 'pera'
+boton.addEventListener("click", comprarVarita);
 
-
-while(darkLord !== 'lord voldemort') {
-  darkLord = prompt(`¿Quién es el líder de los Mortifagos?`).toLowerCase();
-  if (darkLord !== 'lord voldemort') {
-    intentos += ` "${darkLord}"`;
-    count++
-  }
-  if (count >= 3) break;
-};
-
-if (count >= 3) {
-  document.getElementById('intentos').innerHTML = intentos
-  document.getElementById('resultado').classList.add('red')
-  resultado = `¡¡¡AVADA KADABRA!!! ${nombre}, no mereces ser un Mortífago`
-} else {
-  document.getElementById('resultado').classList.add('green')
-  resultado = `¡Bienvenido, ${nombre}!`
+function comprarVarita () {
+  boton.value = "ES MIA"
+  boton.setAttribute("disabled", true)
+  alert("Compraste la varita maldita")
 }
-
-document.getElementById('resultado').innerHTML = resultado
-
