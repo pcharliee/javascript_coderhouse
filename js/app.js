6437ceed820ad6ddc6a3a981b59dd7811e051c7f
Carlos Delgado <carlosm.delgador@gmail.com>
2021-08-01 21:39:47 -0300

    Desafio 14

--- js/app.js
+++ js/app.js
@@ -1,52 +1,37 @@
-// import anime from 'animejs/lib/anime.es.js'
-// const anime = require('animejs');
-
 /* INFORMACION INICIAL PARA FUNCIONAR */
 const spells = [];
 const characters = [];
-const books = [];
-const sectionCards = [
-  {
-    id: 'ollivanders',
-    nombre: 'Ollivanders',
-    descripcion: 'Todo mago necesita una varita mágica. Visita la tienda de Ollivander´s y consigue la tuya.',
-    img: `../media/ollivanders-t.png`,
-
-  },
-  {
-    id: 'quidditch-supplies',
-    nombre: 'Quality Quidditch Supplies',
-    descripcion: 'Ya sea que juegues al Quidditch o la uses para hacer las compras, en Articulos de Calidad para Quidditch tenemos la escoba que se adapta a tus necesidades ',
-    img: `../media/qqs.png`,
-  },
-  {
-    id: 'flourish-blotts',
-    nombre: 'Flourish and Blotts ',
-    descripcion: 'Encuentra todos los libros que necesitas para iniciar tu nuevo año escolar.',
-    img: `../media/flourish.png`,
-  },
-];
+const sectionCards = [];
 
 $('document').ready(() => {
+  // HECHIZOS //
   const API_URL = "https://fedeperin-harry-potter-api.herokuapp.com/db";
-  fetch(API_URL)
-  .then((res) => res.json())
-  .then((data) => { 
-    data.hechizos.map(el => spells.push(el));
-    data.personajes.map(el => characters.push(el));
-  })
-  .then(function () {
-    localStorage.setItem('spells', JSON.stringify(spells))
-    localStorage.setItem('characters', JSON.stringify(characters))
-    console.log('se cargo todo')
-  })
+  $.get(API_URL, (req, res) => {
+    if(res == 'success') {
+      req.hechizos.map(el => spells.push(el));
+      req.personajes.map(el => characters.push(el))
+      localStorage.setItem('spells', JSON.stringify(spells))
+      localStorage.setItem('characters', JSON.stringify(characters))
+    };
+  });
+  
+  // CARDS //
+  const CARDS_JSON = "data/cards.json"
+  $.getJSON(CARDS_JSON, (req, res) => {
+    if(res == 'success') {
+      let cards = req;
+      cards.map(el => sectionCards.push(el));
+      localStorage.setItem('section-cards', JSON.stringify(sectionCards));
+      crearTarjetas(cards)
+    };
+  });
 });
 
 const user = JSON.parse(localStorage.getItem('usuario'));
 const Hechizos = JSON.parse(localStorage.getItem('spells'));
 const Personajes = JSON.parse(localStorage.getItem('characters'));
 
-/* VARIABLES */
+// VARIABLES
 
 let welcome = $('#welcome');
 let sections = $('#sections');
@@ -59,7 +44,7 @@ let userName = !user.nombre ? 'Muggle sin nombre' : user.nombre
 // DOM MANIPULATION
 
 $('body').prepend(`
-  <div id='scroll-to' class='scroll-to'>Scroll down</div>
+<div id='scroll-to' class='scroll-to'>Scroll down</div>
 `);
 cards.addClass('cards-section');
 
@@ -79,17 +64,22 @@ function crearTarjetas (sectionCards) {
       </div>
     </div>`)
   });
+  $('#ollivanders').css({
+    'filter': 'invert(0.75)'
+  });
+  $('#flourish-blotts').css({
+    'filter': 'invert(0.9)'
+  });
 };
 
-crearTarjetas(sectionCards)
 
-$('#ollivanders').css({
-  'filter': 'invert(0.75)'
-});
-$('#flourish-blotts').css({
-  'filter': 'invert(0.9)'
-});
 // DOM INTERACTIONS
+
+welcome.append(`<h2 class='welcome-information-titulo'>Hola, ${userName}!<h2>`);
+welcome.append(`<p class='welcome-information-cta'>
+Da un paseo por nuestro sitio y no te olvides de visitar las tienda exclusiva para magos
+</p>`);
+$('.welcome-information-cta').delay(3000).slideDown(1000);
 
 $('.landing-bg').hover(function () {
   scrolled = false;
@@ -106,12 +96,6 @@ $('.sections-bg').on('mouseenter', function () {
   });
 });
 
-welcome.append(`<h2 class='welcome-information-titulo'>Hola, ${userName}!<h2>`);
-welcome.append(`<p class='welcome-information-cta'>
-Da un paseo por nuestro sitio y no te olvides de visitar las tienda exclusiva para magos
-</p>`);
-
-$('.welcome-information-cta').delay(3000).slideDown(1000)
 
 $('#scroll-to').on('click', function (e) {
   e.preventDefault;
@@ -136,15 +120,15 @@ $(window).on('scroll', function (e) {
 
 var textWrapper = document.querySelector('.welcome-information-titulo');
 textWrapper.innerHTML = textWrapper.textContent
-  .replace(/\S/g, "<span class='letter'>$&</span>");
+.replace(/\S/g, "<span class='letter'>$&</span>");
 anime.timeline({loop: false})
-  .add({
-    targets: '.welcome-information-titulo .letter',
-    opacity: [0,1],
-    easing: 'easeInOutQuad',
-    duration: 1500,
-    delay: (el, i) => 150 * (i+1)
-  });
+.add({
+  targets: '.welcome-information-titulo .letter',
+  opacity: [0,1],
+  easing: 'easeInOutQuad',
+  duration: 1500,
+  delay: (el, i) => 150 * (i+1)
+});
 
 
 
